ef67babdba80923c7679694aceabb1d5eaccfb5c
jethrolibutan <jethrolibutan@gmail.com>
2022-12-20 21:59:58 -0500

    edits to main page

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,15 +1,20 @@
 import Navbar from "../components/Navbar";
 import { Link } from "react-router-dom";
 
+import "./Main.css";
+
 import React from "react";
 
 function Main() {
   return (
-    <div>
-      <Navbar />
-      this the main page yuh
-      <Link to="about"> click to view our about page</Link>
-      <Link to="contact"> click to v iew our contact page</Link>
+    <div style={{ height: window.innerHeight, width: window.innerWidth }}>
+      <div className="navbar">
+        <Navbar />
+      </div>
+
+      <div className="section-1">
+        <div className="section-1-title">farm2clinic</div>
+      </div>
     </div>
   );
 }
