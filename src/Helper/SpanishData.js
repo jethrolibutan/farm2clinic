cc9cc714d5e2ec9fd44249922cbc2529d708bad2
jethrolibutan <jethrolibutan@gmail.com>
2023-01-07 14:36:15 -0500

    prepping spanishdata

---
+++ src/Helper/SpanishData.js
@@ -0,0 +1,206 @@
+import React from "react";
+
+export const sBroccoli = [];
+
+export const cabbage = [
+  {
+    title: "cabbage-benefits",
+    img: Cabbage0,
+  },
+  {
+    title: "cabbage-1",
+    img: Cabbage1,
+  },
+];
+
+export const cantaloupe = [
+  {
+    img: CantaloupeBenefit,
+  },
+  {
+    img: Cant1,
+  },
+  {
+    img: Cant2,
+  },
+  {
+    img: Cant3,
+  },
+];
+
+export const carrot = [
+  {
+    img: CarrotBenefit,
+  },
+  {
+    img: Carr1,
+  },
+  {
+    img: Carr2,
+  },
+  {
+    img: Carr3,
+  },
+];
+
+export const cauliflower = [
+  {
+    img: CauliflowerBenefit,
+  },
+  {
+    img: Caul1,
+  },
+  {
+    img: Caul2,
+  },
+  {
+    img: Caul3,
+  },
+];
+
+export const collard = [
+  {
+    img: CollardBenefit,
+  },
+  {
+    img: Collard1,
+  },
+  {
+    img: Collard2,
+  },
+  {
+    img: Collard3,
+  },
+];
+
+export const cucumber = [
+  {
+    img: CucumberBenefit,
+  },
+  {
+    img: Cuc1,
+  },
+  {
+    img: Cuc2,
+  },
+  {
+    img: Cuc3,
+  },
+];
+
+export const greenBean = [
+  {
+    img: GreenBenefit,
+  },
+  {
+    img: Green1,
+  },
+  {
+    img: Green2,
+  },
+  {
+    img: Green3,
+  },
+];
+
+export const kale = [
+  {
+    img: KaleBenefit,
+  },
+  {
+    img: Kale1,
+  },
+  {
+    img: Kale2,
+  },
+  {
+    img: Kale3,
+  },
+];
+
+export const okra = [
+  {
+    img: OkraBenefit,
+  },
+  {
+    img: Okra1,
+  },
+  {
+    img: Okra2,
+  },
+  {
+    img: Okra3,
+  },
+];
+
+export const spinach = [
+  {
+    img: SpinachBenefit,
+  },
+  {
+    img: Spin1,
+  },
+  {
+    img: Spin2,
+  },
+  {
+    img: Spin3,
+  },
+];
+
+export const squash = [
+  {
+    img: SquashBenefit,
+  },
+  {
+    img: Squash1,
+  },
+  {
+    img: Squash2,
+  },
+  {
+    img: Squash3,
+  },
+];
+
+export const sweetPotato = [
+  { img: SweetBenefit },
+  {
+    img: Sweet1,
+  },
+  {
+    img: Sweet2,
+  },
+  {
+    img: Sweet3,
+  },
+];
+
+export const turnip = [
+  {
+    img: TurnipBenefit,
+  },
+  {
+    img: Turn1,
+  },
+  {
+    img: Turn2,
+  },
+];
+
+export const whitePotato = [
+  {
+    img: WhitePotatoBenefit,
+  },
+  {
+    img: White1,
+  },
+  {
+    img: White2,
+  },
+  {
+    img: White3,
+  },
+];
+
+export default SpanishData;
