72148ed3a83e8e133ddc83c7c30c45dbb5c520f6
jethrolibutan <jethrolibutan@gmail.com>
2023-01-14 13:55:11 -0500

    added training page (under construction)

--- src/pages/Training.js
+++ src/pages/Training.js
@@ -1,11 +1,21 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import Subfooter from "../components/SubFooter";
+import Footer from "../components/Footer";
+import "./Training.css";
 
 function Training() {
   return (
     <div>
       <Navbar />
-      Training
+      <div className="training">Training</div>
+      <div className="under-construction">
+        {" "}
+        This page is under construction!
+      </div>
+
+      <Subfooter />
+      <Footer />
     </div>
   );
 }
