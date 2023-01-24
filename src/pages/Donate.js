abc10ce6d5b4bc0ccdd75a4d85ed662711b73db5
jethrolibutan <jethrolibutan@gmail.com>
2023-01-23 19:41:47 -0500

    added changes to both the getting involved and the donate pages

--- src/pages/Donate.js
+++ src/pages/Donate.js
@@ -1,11 +1,119 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import SubFooter from "../components/SubFooter";
+import Footer from "../components/Footer";
+import "./Donate.css";
 
 function Donate() {
   return (
     <div>
       <Navbar />
-      Donate
+      <div className="donate">Donate</div>
+      <div className="contact-farm-2-clinic"> Contact Farm 2 Clinic! </div>
+      <div className="form-for-contact">
+        <form action="https://formspree.io/f/xrgvbplg" method="post">
+          <fieldset id="fs-frm-inputs">
+            <label for="full-name">Full Name</label>
+            <input
+              type="text"
+              name="name"
+              id="full-name"
+              placeholder="First and Last Name"
+              required=""
+            />
+
+            <label for="email-address">Email Address</label>
+            <input
+              type="email"
+              name="_replyto"
+              id="email-address"
+              placeholder="youremail@domain.com"
+              required=""
+            />
+
+            <label for="message"> Your Message</label>
+            <textarea
+              rows="5"
+              name="message"
+              id="message"
+              placeholder="Type your message here! Contact us with any questions you may have, if you want to get involved, or with any other inquiries you may have!"
+              required=""
+            ></textarea>
+            <input
+              type="hidden"
+              name="_subject"
+              id="email-subject"
+              value="Contact Form Submission"
+            />
+
+            <input type="submit" value="Submit" />
+          </fieldset>
+        </form>{" "}
+      </div>
+      <div claassName="get-involved-container">
+        <div className="follow-us-section">
+          {" "}
+          <div className="follow-us-title">Follow us on social media!</div>
+          <div className="social-media-rows">
+            <div className="social-media">
+              {" "}
+              <a
+                href="https://www.youtube.com/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src="./images/facebookicon.png" alt="Watch Clinic logo" />
+              </a>
+            </div>{" "}
+            <div className="social-media">
+              {" "}
+              <a
+                href="https://instagram.com/farm2clinic/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src="./images/instagramicon.png" alt="Watch Clinic logo" />
+              </a>
+            </div>
+          </div>
+          <div className="social-media-rows">
+            <div className="social-media">
+              <a
+                href="https://twitter.com/farm2clinic"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src="./images/twitterIcon.png" alt="Watch Clinic logo" />
+              </a>
+            </div>{" "}
+            <div className="social-media">
+              <a
+                href="https://youtube.com/channel/UCpuQAa3TLdvy8VKbUpnIVkA"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src="./images/youtubeicon.png" alt="Watch Clinic logo" />
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="container-map">
+          <iframe
+            width="399"
+            height="542"
+            frameborder="0"
+            marginheight="0"
+            marginwidth="0"
+            id="gmap_canvas"
+            src="https://maps.google.com/maps?width=399&amp;height=542&amp;hl=en&amp;q=Health%20Sciences%20Building,%20Greenville%20Greenville+(Health%20Sciences%20Building)&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+            className="map-container"
+            title="map"
+          ></iframe>
+        </div>
+
+        <SubFooter />
+        <Footer />
+      </div>
     </div>
   );
 }
--- src/pages/GetInvolved.js
+++ src/pages/GetInvolved.js
@@ -2,7 +2,6 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import SubFooter from "../components/SubFooter";
 import Footer from "../components/Footer";
-
 import "./GetInvolved.css";
 
 function GetInvolved() {
@@ -11,106 +10,99 @@ function GetInvolved() {
       <Navbar />
       <div className="get-involved">Get Involved</div>
 
-      <div className="contact-farm-2-clinic"> Contact Farm 2 Clinic! </div>
-      <div className="form-for-contact">
-        <form action="https://formspree.io/f/xrgvbplg" method="post">
-          <fieldset id="fs-frm-inputs">
-            <label for="full-name">Full Name</label>
-            <input
-              type="text"
-              name="name"
-              id="full-name"
-              placeholder="First and Last Name"
-              required=""
-            />
-
-            <label for="email-address">Email Address</label>
-            <input
-              type="email"
-              name="_replyto"
-              id="email-address"
-              placeholder="youremail@domain.com"
-              required=""
-            />
-
-            <label for="message"> Your Message</label>
-            <textarea
-              rows="5"
-              name="message"
-              id="message"
-              placeholder="Type your message here! Contact us with any questions you may have, if you want to get involved, or with any other inquiries you may have!"
-              required=""
-            ></textarea>
-            <input
-              type="hidden"
-              name="_subject"
-              id="email-subject"
-              value="Contact Form Submission"
-            />
-
-            <input type="submit" value="Submit" />
-          </fieldset>
-        </form>{" "}
-      </div>
-
-      <div claassName="get-involved-container">
-        <div className="follow-us-section">
-          {" "}
-          <div className="follow-us-title">Follow us on social media!</div>
-          <div className="social-media-rows">
-            <div className="social-media">
-              {" "}
-              <a
-                href="https://www.youtube.com/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <img src="./images/facebookicon.png" alt="Watch Clinic logo" />
-              </a>
-            </div>{" "}
-            <div className="social-media">
-              {" "}
-              <a
-                href="https://instagram.com/farm2clinic/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <img src="./images/instagramicon.png" alt="Watch Clinic logo" />
-              </a>
-            </div>
-          </div>
-          <div className="social-media-rows">
-            <div className="social-media">
-              <a
-                href="https://twitter.com/farm2clinic"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <img src="./images/twitterIcon.png" alt="Watch Clinic logo" />
-              </a>
-            </div>{" "}
-            <div className="social-media">
-              <a
-                href="https://youtube.com/channel/UCpuQAa3TLdvy8VKbUpnIVkA"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <img src="./images/youtubeicon.png" alt="Watch Clinic logo" />
-              </a>
-            </div>
-          </div>
+      <div className="getting-involved-container">
+        <div className="getting-involved-intro">
+          If you want to get involved with Farm2Clinic, keep reading!
         </div>
-        <div className="container-map">
-          <iframe
-            width="399"
-            height="542"
-            frameborder="0"
-            marginheight="0"
-            marginwidth="0"
-            id="gmap_canvas"
-            src="https://maps.google.com/maps?width=399&amp;height=542&amp;hl=en&amp;q=Health%20Sciences%20Building,%20Greenville%20Greenville+(Health%20Sciences%20Building)&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
-            className="map-container"
-          ></iframe>
+
+        <div className="getting-involved-community">
+          <div className="getting-involved-list">
+            <h1 className="community-title">
+              {" "}
+              Community memebers can get involved by:{" "}
+            </h1>
+
+            <p className="community-involvement-section">
+              <ul className="community-involvement">
+                <li>
+                  Attend a gleaning and support the harvesting and collection of
+                  healthful food that goes to the patients.{" "}
+                </li>
+                <li>Assist with loading/unloading the mobile unit. </li>
+                <li>
+                  Assist with interpreting (Spanish) at classes, enrollment
+                  visits, and/or translation.{" "}
+                </li>
+                <li>
+                  See the{" "}
+                  <a
+                    href="https://farm2clinic.com/donate"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    {" "}
+                    donate page
+                  </a>{" "}
+                  - for other direct ways to support!{" "}
+                </li>
+              </ul>
+            </p>
+
+            <h1 className="student-title"> Students can get involved by: </h1>
+
+            <p className="student-involvement-section">
+              <ul className="student-involvement">
+                <li>
+                  Train as a student health coach to gain 1 on 1 experience
+                  providing patient education and counseling and accumulate
+                  patient contact hours- can be done on a volunteer basis or for
+                  course/internship credits. <br></br>
+                  <a
+                    href="https://docs.google.com/forms/d/e/1FAIpQLScDx0j0OyllyJy0DRkHduQMKqyw2AUo0c1xxYJRIOevEKuUgA/viewform"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Click on me to apply!
+                  </a>
+                </li>
+                <li>
+                  {" "}
+                  Research Experience (Apply for an URCA to support you to work
+                  with us for a Signature Honors Project, or capstone/Thesis
+                  project). <br />
+                  <a
+                    href="https://rede.ecu.edu/undergraduate/undergraduate-research-and-creative-activity-awards/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    {" "}
+                    Click on me to apply for an UCRA to support you to work with
+                    us!
+                  </a>{" "}
+                </li>
+                <li>
+                  Gain experience delivering the on-site group classes with
+                  nutrition and health education, physical activity
+                  demonstrations as well as healthy cooking demonstrations- can
+                  be done as a volunteer basis or for course/internship credits.{" "}
+                  <br />
+                  <a
+                    href="https://docs.google.com/forms/d/e/1FAIpQLScDx0j0OyllyJy0DRkHduQMKqyw2AUo0c1xxYJRIOevEKuUgA/viewform"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    {" "}
+                    Click on me to apply!
+                  </a>{" "}
+                </li>
+                <li>
+                  **Student volunteers are typically given preference for
+                  potential paid assistantship positions (undergraduate and/or
+                  graduate positions).
+                </li>
+              </ul>
+            </p>
+          </div>
         </div>
       </div>
 
