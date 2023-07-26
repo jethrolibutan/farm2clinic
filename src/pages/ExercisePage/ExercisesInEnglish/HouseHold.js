5d8480241c9b86a3d5881d18214a56d39107b7b7
jethrolibutan <jethrolibutan@gmail.com>
2023-07-26 18:51:03 -0400

    Added  content into English Physical Activities

--- src/pages/ExercisePage/ExercisesInEnglish/HouseHold.js
+++ src/pages/ExercisePage/ExercisesInEnglish/HouseHold.js
@@ -1,7 +1,51 @@
 import React from "react";
+import Navbar from "../../../components/Navbar";
+import SubFooter from "../../../components/SubFooter";
+import Footer from "../../../components/Footer";
+import { Link, Button } from "@mui/material";
 
 function HouseHold() {
-  return <div>HouseHold</div>;
+  return (
+    <div>
+      {" "}
+      <Navbar />
+      <div className="exercises">
+        {" "}
+        Physical Activity Resources
+        <div className="">
+          <Button
+            variant="contained"
+            component={Link}
+            href="/exercises"
+            sx={{ mt: "20px", ml: "10px", mr: "10px" }}
+          >
+            {" "}
+            Exercises in English{" "}
+          </Button>
+          <Button
+            variant="contained"
+            component={Link}
+            href="/exerciseVideosInEnglish"
+            sx={{ mt: "20px", ml: "10px", mr: "10px" }}
+          >
+            {" "}
+            Exercise Videos in English{" "}
+          </Button>
+          <Button
+            variant="contained"
+            component={Link}
+            href="/exercisesInSpanish"
+            sx={{ mt: "20px", ml: "10px", mr: "10px" }}
+          >
+            {" "}
+            Exercises in Spanish{" "}
+          </Button>
+        </div>
+      </div>
+      <SubFooter />
+      <Footer />
+    </div>
+  );
 }
 
 export default HouseHold;
