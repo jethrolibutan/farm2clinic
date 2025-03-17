47052cc1131201aa40e3faea6b6955cd1995ce7a
jethrolibutan <jethrolibutan@gmail.com>
2025-03-17 12:31:31 -0400

    Removed navbar, added redirecting link!

--- src/pages/MainPage/Main.js
+++ src/pages/MainPage/Main.js
@@ -1,6 +1,7 @@
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
 import { Button } from "@mui/material";
+import { Link } from "@mui/material";
 import SubFooter from "../../components/SubFooter";
 
 import "./Main.css";
@@ -10,25 +11,24 @@ import React from "react";
 function Main() {
   return (
     <div style={{ height: window.innerHeight, width: window.innerWidth }}>
-      <div className="navbar">
+      {/* <div className="navbar">
         <Navbar />
-      </div>
+      </div> */}
       <div className="section-1">
         <div className="box">
-          <div className="farm2clinic">FARM 2 CLINIC</div>
+          <div className="farm2clinic">We've Moved!</div>
           <div className="section-1-title-text">
-            {" "}
-            Adressing health and social barriers to nutrition and health{" "}
+            Check Us Out at Our New Website Here:
           </div>
         </div>
 
         <div>
           <Button
             variant="contained"
-            href="/whatWeDo"
+            href="https://farm2clinicwebsite.weebly.com"
             sx={{ m: "2", mt: "25px", fontSize: "21px" }}
           >
-            LEARN MORE
+            Click Me!
           </Button>
         </div>
       </div>
